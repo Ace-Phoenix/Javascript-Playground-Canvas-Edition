db9bb259960c6fabb6810bc6cf5b30b0096a529c
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2020-12-08 11:32:01 -0500

    ok so i did the border and movement check so yeah
    All that needs to be done is that the corner check hasn't been made so im leaving that to group work

--- canvas.js
+++ canvas.js
@@ -1,31 +1,31 @@
 
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-
-setInterval(function(){
-ctx.beginPath() ;
-var pos = {x:Math.floor(Math.random()*400), y:Math.floor(Math.random()*300), r:Math.floor(Math.random()*90)+4};
-if ((pos.x-pos.r) < 0) {
-    pos.x+=pos.r;
-}
-else if ((pos.x+pos.r) > 400) {
-    pos.x-=pos.r;
-}
-if ((pos.y-pos.r) < 0) {
-    pos.y += pos.r;
-}
-else if ((pos.y + pos.r) > 300) {
-    pos.y-=pos.r;
-}
-var loops = ["circle", "rect"];
-for (var i = 0; i < 100; i++) {
-    var rand = Math.floor(Math.random()*2);
-    var t = loops[rand];
-}
-var asseter = new Asset(t, {x:pos.x, y:pos.y}, pos.r, Math.floor(Math.random()*10)+2);
-asseter.draw();
-}, 100);
-setInterval(function(){ctx.clearRect(0,0,400, 300);},2000);
+//
+//setInterval(function(){
+//ctx.beginPath() ;
+//var pos = {x:Math.floor(Math.random()*400), y:Math.floor(Math.random()*300), r:Math.floor(Math.random()*90)+4};
+//if ((pos.x-pos.r) < 0) {
+//    pos.x+=pos.r;
+//}
+//else if ((pos.x+pos.r) > 400) {
+//    pos.x-=pos.r;
+//}
+//if ((pos.y-pos.r) < 0) {
+//    pos.y += pos.r;
+//}
+//else if ((pos.y + pos.r) > 300) {
+//    pos.y-=pos.r;
+//}
+//var loops = ["circle", "rect"];
+//for (var i = 0; i < 100; i++) {
+//    var rand = Math.floor(Math.random()*2);
+//    var t = loops[rand];
+//}
+//var asseter = new Asset(t, {x:pos.x, y:pos.y}, pos.r, Math.floor(Math.random()*10)+2);
+//asseter.draw();
+//}, 100);
+setInterval(function(){ctx.clearRect(0,0,400, 400);},20);
 
 
 
@@ -51,30 +51,38 @@ class Asset {
 
     }
 }
-var bouncyBall = new Asset("circle", {x:10,y:10}, 10, 5, "south-east");
+var bouncyBall = new Asset("circle", {x:20,y:20}, 10, 5, "south-east");
 function bouncyBouncy() {
-    setInterval(bouncy, 100);
+    setInterval(bouncy, 10);
     function bouncy() {
-        if ((bouncyBall.center.y + bouncyBall.num1) == c.height) {
-            bouncyBall = new Asset("Circle", bouncyBall.center, bouncyBall.num1, bouncyBall.num2, "north-east");
+        if ((bouncyBall.center.x + bouncyBall.num1) == c.width) {
+            bouncyBall = new Asset("Circle", bouncyBall.center, bouncyBall.num1, bouncyBall.num2, "north-west");
         }else if (bouncyBall.direction == "north-east") {
-            bouncyBall = new Asset("Circle", {x:bouncyBall.center.x+5,y:bouncyBall.center.y});
+            bouncyBall = new Asset("Circle", {x:bouncyBall.center.x+1,y:bouncyBall.center.y-1},bouncyBall.num1, bouncyBall.num2, "north-east");
         }
         if ((bouncyBall.center.y - bouncyBall.num1) == 0) {
             bouncyBall = new Asset("Circle", bouncyBall.center, bouncyBall.num1, bouncyBall.num2, "south-west");
-        }else if (bouncyBall.direction == "south-west") {
-            //So that im not just doing it all myself im deciding to have the rest of you figure out how to
-            // / what to put in here and the two bellow good luck
+        }else if (bouncyBall.direction == "south-east") {
+                       bouncyBall = new Asset("Circle", {x:bouncyBall.center.x+1, y:bouncyBall.center.y+1}, bouncyBall.num1, bouncyBall.num2, "south-east");
         }
-        if ((bouncyBall.center.x + bouncyBall.num1) == c.width) {
-            bouncyBall = new Asset("Circle", bouncyBall.center, bouncyBall.num1, bouncyBall.num2, "north-west");
-        }//here we need a else if statement  
-        if ((bouncyBall.center.x - bouncyBall.num1) == 0) {
+        if ((bouncyBall.center.x - bouncyBall.num1) <= 0) {
             bouncyBall = new Asset("Circle", bouncyBall.center, bouncyBall.num1, bouncyBall.num2, "south-east");
-        }//as well as here 
-        
+        }else if (bouncyBall.direction == "north-west") {
+            bouncyBall = new Asset("Circle", {x:bouncyBall.center.x-1, y:bouncyBall.center.y-1}, bouncyBall.num1, bouncyBall.num2, "north-west");
+        }
+        if ((bouncyBall.center.y - bouncyBall.num1) == c.height) {
+            bouncyBall = new Asset("Circle", bouncyBall.center, bouncyBall.num1, bouncyBall.num2, "north-east");
+        }else if (bouncyBall.direction == "south-west") {//down right
+                    bouncyBall = new Asset("Circle", {x:bouncyBall.center.x-1, y:bouncyBall.center.y+1}, bouncyBall.num1, bouncyBall.num2, "south-west");
+}
+        bouncyBall.draw();
     }
 }
+bouncyBouncy();
+// im going to have you guys figure out how to make the circle work with corners because currently it has
+//sad when it gets to a corner also this shows when i changed the canvas to 400 400 so ge to it i guess 
+
+
 //few questions to answer because i want to know if you all know how my class works there are three questions just
 //make another space put your name and then your answer
 
