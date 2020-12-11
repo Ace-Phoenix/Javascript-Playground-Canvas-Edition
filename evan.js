efa8cdd2de4bd3547ee69aa469f9e5e6e7c36aee
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2020-12-11 12:11:39 -0500

    Don't question MY MADNESS... just embrace it
    No but for real i made rainbows yes i said rainbows why because why not there are two small check boxes that toggle on and off rainbow ball and rainbow lines

--- evan.js
+++ evan.js
@@ -1,6 +1,7 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-
+var rl = document.getElementById("rainbowLines");
+var rb = document.getElementById("rainbowBall");
 /*
 var x = c.width / 20; //These 2 variables determine the starting circles location, in this case, the top right of the screen.
 var y = c.height / 20;
@@ -14,14 +15,25 @@ var ballSize = Math.floor(Math.random() * size - 5) + 6; //Sets the circle's rad
 */
 var size = (Math.abs(c.width - c.height)) / 10;
 if (size == 0) { size = 25; };
-var one = 2;
-var two = 4;
+var one = Math.floor(Math.random()*13)+2;
+var two = Math.floor(Math.random()*11)+4;
 var ball = {x:c.width / 20, y:c.height / 20, dx:one, dy:two, ballSize:Math.floor(Math.random() * size - 5) + 6};
 
 function drawCircle() {
+ var colors = ["red", "green", "blue", "violet", "pink", "cyan", "yellow", "orange"];
+   var rainbow = rb.checked;
  ctx.beginPath();
  ctx.arc(ball.x, ball.y, ball.ballSize, 0, Math.PI*2); //The circle, on frame one, will always start in the middle, and its size will always be set to ballSize.
- ctx.fillStyle = "#0095DD"; //Sets the color of the circle to light blue.
+   if (rainbow == true) {
+    //code
+  for (var j = 0; j< 10; j++) {
+    var color = colors[Math.floor(Math.random()*colors.length)];
+  }
+  ctx.fillStyle = color;
+  }else{
+   ctx.fillStyle = "#0095DD"; //Sets the color of the circle to light blue.
+  }
+
  ctx.fill(); //Fills in the circle with the color provided in fillStyle.
  ctx.stroke();
 }
@@ -29,6 +41,8 @@ function drawCircle() {
 
 function lineDraw() {
   ctx.beginPath();
+  var rainbow = rl.checked;
+  var colors = ["red", "green", "blue", "violet", "pink", "cyan", "yellow", "orange"];
   for (var i = lines.length - 1; i >= 0; i--) {
   if (i == lines.length-1) {
     ctx.moveTo(ball.x,ball.y);
@@ -39,12 +53,23 @@ function lineDraw() {
     ctx.lineTo(lines[i+1].x,lines[i+1].y);
     
   }
+  if (rainbow == true) {
+    //code
+  for (var j = 0; j< 10; j++) {
+    var color = colors[Math.floor(Math.random()*colors.length)];
   }
+  ctx.strokeStyle = color;
+  }else{
+  ctx.strokeStyle = "black";   
+  }
+  }
+  
   ctx.stroke();
 }
 
 function draw() {
  ctx.clearRect(0, 0, c.width, c.height); //Clears the canvas every frame, so a new circle can be drawn.
+ lineDraw();
  drawCircle();
  if((ball.x + ball.dx > c.width - ball.ballSize) || (ball.x + ball.dx < ball.ballSize)) { //If the circle's x position exceeds the width of the canvas...
    ball.dx = -ball.dx; //Its x direction will be flipped.
@@ -58,7 +83,6 @@ function draw() {
 
  ball.x += ball.dx;
  ball.y += ball.dy;
- lineDraw();
 }
 
-setInterval(draw, 10);
+setInterval(draw, 25);
