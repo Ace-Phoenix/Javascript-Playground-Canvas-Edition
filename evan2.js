bf37e58c21317427794fbc0eb9eeb6898cb6faf8
Euan Barclay <Silverswimmer63@gmail.com>
2020-12-22 11:08:28 -0500

    changed some spacing and channged damping to .75

--- evan2.js
+++ evan2.js
@@ -8,7 +8,7 @@ var dx = 10; //These variables will be used later to change the position of the 
 var dy = 10; //Changing both of these numbers will also change the speed of the circle (in other words, how many units the circle moves per frame).
 
 var gravity = 0.2; //Will determine how fast the ball falls to the ground.
-var damping = 0.8; //Will set how high the ball bounces.
+var damping = 0.75; //Will set how high the ball bounces.
 var traction = 0.8; //When the ball hits the ground, it will either gain or lose some speed. If above 1, it will speed up. If below 1, it will slow down.
 
 var ballSize = 20; //Sets the circle's radius.
@@ -24,23 +24,16 @@ function drawCircle() {
 function draw() {
   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height); //Clears the canvas every frame, so a new circle can be drawn.
   drawCircle();
-
   if (x + dx > myCanvas.width - ballSize || x + dx < ballSize) { //If the circle's x position exceeds the width of the canvas...
     dx = -dx * damping; //The ball's x direction will be flipped, and it will bounce a specific distance (damping).
   }
-
   if (y + dy > myCanvas.height - ballSize || y + dy < ballSize) { //If the circle's y position exceeds the height of the canvas...
     dy = -dy * damping; //The ball's y direction will be flipped, and it will bounce a specfic height (damping).
     dx *= traction; //When the ball hits the ground, it will lose/gain some momentum based on the traction value.
   }
-
   dy += gravity; //Adds the gravity value to the ball's dy value, giving it a artificial force of gravity.
-
   x += dx;
-
-  if (((y + dy) + ballSize) <= 300) {
-    y += dy;
-  }
+  if (((y + dy) + ballSize) <= 300) { y += dy; }
 }
 
 setInterval(draw, 10);
