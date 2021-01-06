024124579d3dda12ef9c446381d7c1efa4a00f6c
Fly2Freedom <71277657+Fly2Freedom@users.noreply.github.com>
2021-01-06 11:34:10 -0500

    first step to making pipes

--- evan2.js
+++ evan2.js
@@ -1,20 +1,20 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-var x = myCanvas.width / 20; //These 2 variables determine the starting circles location, in this case, the top left of the screen.
-var y = myCanvas.height / 20;
+var x = myCanvas.width / 2; //These 2 variables determine the starting circles location, in this case, the middle of the screen.
+var y = myCanvas.height / 2;
 
 var dx = 10; //These variables will be used later to change the position of the circle.
 var dy = 10; //Changing both of these numbers will also change the speed of the circle (in other words, how many units the circle moves per frame).
 
 var gravity = 0.2; //Sets the gravity pulling the ball to the ground.
-var damping = 0.75; //The rate at which the ball slows down.
+var damping = 0.01; //The rate at which the ball slows down.
 var traction = 0.95; //Will make the ball stop.
 var ballSize = 20; //Sets the circle's radius.
 
 function drawCircle() {
   ctx.beginPath();
-  ctx.arc(x, y, ballSize, 0, Math.PI*2); //The circle, on frame one, will always start at the top left, and its size will always be set to ballSize.
+  ctx.arc(x, y, ballSize, 0, Math.PI*2); //The circle, on frame one, will always start in the middle, and its size will always be set to ballSize.
   ctx.fillStyle = "#0095DD"; //Sets the color of the circle to light blue.
   ctx.fill(); //Fills in the circle with the color provided in fillStyle.
   ctx.stroke();
@@ -35,7 +35,7 @@ function draw() {
 
   dy += gravity; //Adds the gravity value to the ball's dy value, giving it a artificial force of gravity.
 
-  x += dx;
+  //x += dx;
 
   if (((y + dy) + ballSize) <= 300) {
     y += dy;
@@ -56,3 +56,17 @@ function keyPress(e) { //Function that will play out when a key is pressed (e is
       dy-=10; //Will make the ball jump a small distance.
     }
 }
+
+function lines() {
+  ctx.beginPath();
+  ctx.moveTo(550, 0);
+  ctx.lineTo(550, 350);
+  ctx.stroke();
+}
+
+function drawLines() {
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+  lines();
+}
+
+setInterval(drawLines, 9);
