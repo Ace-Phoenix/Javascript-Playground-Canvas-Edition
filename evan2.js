b881f6d7630eac15e5b3e0b59cc0595adec4dfee
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2021-01-06 12:21:12 -0500

    I have the pipes moving just need to add collition and add a top to them

--- evan2.js
+++ evan2.js
@@ -7,57 +7,42 @@ var y = myCanvas.height / 2;
 var dx = 10; //These variables will be used later to change the position of the circle.
 var dy = 10; //Changing both of these numbers will also change the speed of the circle (in other words, how many units the circle moves per frame).
 
+function getAllCoords(p) {
+    
+}
 
 
 // @function exeptionsRand();
-// @param exeption [integer] : telles the function when to start excluding
-// @param sign [string] {Expects} ["<",">","<=",">="]: tells the function what signs to use
+// @param inOrOut [string] {Expects} ["in","out"]: Tells the function weather or not to use out or in for the ifs
 // @param max [integer] {requires} [max > min] : tells the function when to stop;
 // @param min [integer] {requires} [min < max] : tells the function when to start;
-function exeptionsRand(exeption, sign, max,min) {
-    var randomInitial = Math.floor(Math.random()*max);
-    var acceptable = false;
-    while (acceptable == false) {
-    if (sign == ">") {
-        if (randomInitial < exeption) {
-            acceptable = true;
-        }else{          
-          randomInitial = Math.floor(Math.random()*max);
-        }
-    }else if (sign == "<") {
-        if (randomInitial > exeption) {
-            acceptable = true;
-        }else{          
-          randomInitial = Math.floor(Math.random()*max);
-        }
+function exeptionsRand(intOrOut,max,min) {
+    var inBool = true;
+  while (inBool == true) {
+  var randNumber = Math.floor(Math.random()*(max-min))+min;
+  if (intOrOut == "in") {
+    if (max >= randNumber && randNumber >= min) {
+        inBool = false;
+    }else{
+    randNumber = Math.floor(Math.random()*(max-min)) + min;      
     }
-    else if (sign == "<=") {
-        if (randomInitial >= exeption) {
-            acceptable = true;
-        }else{
-          randomInitial = Math.floor(Math.random()*max);
-        }
+  }else if (intOrOut == "out") {
+    if (min >= randNumber <= max) {
+        inBool = false;
+    }else{
+          randNumber = Math.floor(Math.random()*max)-min;
+
     }
-    else if (sign == ">=") {
-        if (randomInitial <= exeption) {
-            acceptable = true;
-        }
-        else{
-            randomInitial = Math.floor(Math.random()*max);
-        }
-    }
-    else{
-      throw new Error("in exeptionsRand() sign expects to be >, <, <=, >= and got " + sign + " please chaneg the sign to match expectations");
-    }
-    }
-    return randomInitial;
+    //code
+  }
+  }
+  return randNumber;
 }
-var pipx = exeptionsRand(15, "<",myCanvas.width-15,0);
-var pipy = exeptionsRand(15, "<",Math.floor(myCanvas.height/3)-15,0);
-var pipeW1 = exeptionsRand(28, ">", 50,0);
-var pipeH1 = exeptionsRand(36, ">", 2456,0);
-var pipBotObj = {width:pipeW1,height:234, x:pipx ,y:pipy+460,gap: 20}; 
-var pipTopObj = {width: pipeW1,height:pipBotObj.gap, x:pipx,y:0};
+var pipeWalls = {north:undefined,east:undefined,south:undefined,west:undefined};
+var pipx = exeptionsRand("in", myCanvas.width-30, 75);
+var pipy = exeptionsRand("in", myCanvas.height-35,90);
+var pipBotObj = {width:45,height:450, x:pipx ,y:pipy,gap: 40};
+var pipTopObj = {width: 45,height:pipy-(pipBotObj.gap*2), x:pipx,y: 0};
 
 
 var gravity = 0.2; //Sets the gravity pulling the ball to the ground.
@@ -66,8 +51,18 @@ var traction = 0.95; //Will make the ball stop.
 var ballSize = 20; //Sets the circle's radius.
 function drawPipes(){
     ctx.beginPath();
-    ctx.rect(pipBotObj.width, pipBotObj.height, pipBotObj.x,pipBotObj.y);
-    ctx.rect(pipTopObj.width, pipTopObj.height, pipTopObj.x, pipTopObj.y);
+    if ((pipTopObj.x +pipTopObj.width) == 0) {
+        pipBotObj.x = myCanvas.width;
+        pipTopObj.x = myCanvas.width;
+        pipx = exeptionsRand("in", myCanvas.width-30, 75);
+        pipy = exeptionsRand("in", myCanvas.height-75,46);
+      pipBotObj.y = pipy;
+      pipTopObj.height = pipy-(pipBotObj.gap*2);
+    }
+    pipBotObj.x = pipBotObj.x - 1;
+    pipTopObj.x = pipTopObj.x - 1;
+    ctx.rect(pipBotObj.x, pipBotObj.y, pipBotObj.width, pipBotObj.height);
+    ctx.rect(pipTopObj.x, pipTopObj.y, pipTopObj.width, pipTopObj.height);
   ctx.stroke();
 }
 
@@ -85,7 +80,6 @@ function draw() {
   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height); //Clears the canvas every frame, so a new circle can be drawn.
   drawCircle();
 drawPipes();
-lines();
 
   if (x + dx > myCanvas.width - ballSize || x + dx < ballSize) { //If the circle's x position exceeds the width of the canvas...
     dx = -dx * damping; //The ball's x direction will be flipped, and it will bounce a specific distance (damping).
@@ -120,11 +114,5 @@ function keyPress(e) { //Function that will play out when a key is pressed (e is
     }
 }
 
-function lines() {
-  ctx.beginPath();
-  ctx.moveTo(550, 0);
-  ctx.lineTo(550, 350);
-  ctx.stroke();
-}
 
 
